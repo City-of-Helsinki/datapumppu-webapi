cf0ef6d2e012429a4142ae7b57afc9a883fafbb3
Vappu Koskela <vappu.koskela@ajrsolutions.fi>
2022-12-22 08:50:49 +0200

    SignalR Client connection

--- WebAPI/WebComponents/Meeting/src/Meeting.js
+++ WebAPI/WebComponents/Meeting/src/Meeting.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import AgendaItem from './AgendaItem'
 export default function Meeting() {
   const [agenda, setAgenda] = React.useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('https://dev.hel.fi:443/paatokset/v1/agenda_item/?meeting=69977') // old api
@@ -12,6 +13,25 @@ export default function Meeting() {
 
     fetchData()
   }, [setAgenda])
+
+  useEffect(() => {
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl("#--API_URL--#/live")
+      .withAutomaticReconnect()
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    connection.start().then(result => {
+
+      console.log("Connected");
+      connection.on("ReceiveMessage", message => {
+        console.log("Message received; ", message)
+      })
+
+
+    }).catch(err => console.log(err))
+  },  [])
+
 
   return (
     <div className="container">
