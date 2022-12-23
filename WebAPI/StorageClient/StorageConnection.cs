9fec66d89850f61e864efe2245f4aec7674adb8c
Veli-Matti Tikkanen <veli-matti.tikkanen@avanio.com>
2022-12-23 09:43:54 +0200

    added StorageClient

---
+++ WebAPI/StorageClient/StorageConnection.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAPI.StorageClient
+{
+    public interface IStorageConnection
+    {
+        HttpClient CreateConnection();
+    }
+
+    public class StorageConnection : IStorageConnection
+    {
+        private readonly IConfiguration _configuration;
+
+        public StorageConnection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public HttpClient CreateConnection()
+        {
+            var connection = new HttpClient();
+            connection.BaseAddress = new Uri(_configuration["Storage:url"]);
+            return connection;
+        }
+    }
+}
