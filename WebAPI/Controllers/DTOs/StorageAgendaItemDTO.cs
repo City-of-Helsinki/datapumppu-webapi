1620e9dcfe6d4b71d5d5412dc10473f97a0a675d
Veli-Matti Tikkanen <veli-matti.tikkanen@avanio.com>
2022-12-23 09:43:12 +0200

    added StorageDTOs

---
+++ WebAPI/Controllers/DTOs/StorageAgendaItemDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPI.Controllers.DTOs
+{
+    public class StorageAgendaItemDTO
+    {
+        public int AgendaPoint { get; set; }
+
+        public string? Section { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? CaseIDLabel { get; set; }
+
+        public string? Html { get; set; }
+
+        public string? DecisionHistoryHTML { get; set; }
+    }
+}
