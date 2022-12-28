a461044a029fc6ad59c68cb16f115bc76afe68d0
Ville Palo <ville.palo@avanio.com>
2022-12-28 16:20:30 +0200

    hkd-45 fetch and show decisions page

---
+++ WebAPI/Controllers/DecisionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers.DTOs;
+using WebAPI.Controllers.Filters;
+using WebAPI.StorageClient;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("decisions")]
+    [TypeFilter(typeof(WebAPIExceptionFilter))]
+    public class DecisionsController: ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IStorageApiClient _storageApiClient;
+        private readonly ILogger<MeetingController> _logger;
+
+        public DecisionsController(IConfiguration configuration, IStorageApiClient storageApiClient, ILogger<MeetingController> logger)
+        {
+            _configuration = configuration;
+            _storageApiClient = storageApiClient;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{caseLabelId}/{language}")]
+        public async Task<IActionResult> GetDecision(string caseLabelId, string language)
+        {
+            _logger.LogInformation("Executing GetDecision()");
+            var decision = await _storageApiClient.RequestDecision(caseLabelId, language.Substring(0, 2));
+            if (decision== null)
+            {
+                return NoContent();
+            }
+
+            return new OkObjectResult(decision);
+        }
+    }
+}
--- WebAPI/WebComponents/Decision/src/Decision.js
+++ WebAPI/WebComponents/Decision/src/Decision.js
@@ -5,25 +5,37 @@ import ReactDOM from 'react-dom'
 export default function Decision () {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = React.useState(0)
-  const [buttonText, setButtonText] = React.useState('decision button text')
-  const message = `You clicked ${count} times`
+  const [decisionHtml, setDecisionHtml] = React.useState('')
+  const [attachments, setAttachments] = React.useState(undefined)
+  const [title, setTitle] = React.useState('')
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('#--API_URL--#/api/buttontext')
-      const json = await response.json()
-      setButtonText(json.text)
+      const response = await fetch('#--API_URL--#/decisions/#--CASE_ID_LABEL--#/#--LANG--#"')
+      const decision = await response.json()
+      setDecisionHtml(decision.motion)
+      setAttachments(decision.attachments)
+      setTitle(decision.title)
     }
-
     fetchData()
-  }, [setButtonText])
+  }, [setDecisionHtml])
+
+  console.log("title", title)
 
   return (
     <div>
-      <p>{message}</p>
-      <button type='button' onClick={() => setCount(count + 1)}>
-        {buttonText}
-      </button>
+      <h3>{title}</h3>
+      <div>
+        {decisionHtml && <div dangerouslySetInnerHTML={{ __html: decisionHtml }} />}
+      </div>
+      <h4>{"#--LANG--#" === "fi" ? "Linkit" : "Hyperlänkar"}</h4>
+      <div>
+        <ul>
+        {attachments && attachments.map((attachment, index) =>          
+          <li key={index}><a href={attachment.fileURI}>{attachment.title}</a></li>
+        )}
+        </ul>
+      </div>
     </div>
   )
 }
