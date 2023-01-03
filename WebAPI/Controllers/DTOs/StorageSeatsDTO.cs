3ead8589ab50afa9b98d74e654687372c78dbf98
Ville Palo <ville.palo@avanio.com>
2023-01-03 08:46:09 +0200

    hkd-82 fetching seats from database

---
+++ WebAPI/Controllers/DTOs/StorageSeatsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Controllers.DTOs
+{
+    public class SeatDTO
+    {
+        public string? PersonFI { get; set; }
+
+        public string? PersonSV { get; set; }
+
+        public string? SeatId { get; set; }
+    }
+
+}
--- WebAPI/WebComponents/Meeting/src/SeatMap.js
+++ WebAPI/WebComponents/Meeting/src/SeatMap.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import SeatRow from './SeatRow'
 
 const champerStyle = {
@@ -11,110 +11,53 @@ const champerStyle = {
     padding: 0
 }
 
-export default function SeatMap() {
+export default function SeatMap(props) {
+    const [seats, setSeats] = useState([]);
+    const [seatMap, setSeatMap] = useState([]);
 
-    // seats for testing purposes
-    const seats = [
-        {},
-        {
-            seat: 1,
-            name: "Ällikki Runo (Kok.)",
-        },
-        {
-            seat: 2,
-            name: "Ripi Mitja (Saml.)",
-        },
-        {
-            seat: 3,
-            name: "Lappa Era (Kok)",
-        },        
-        {
-            seat: 5,
-            name: "Sasa Raisu (PerusS)",
-        },
-        {
-            seat: 6,
-            name: "Mitja Viima (SFP)",
-        },        
-        {
-            seat: 7,
-            name: "Killi Sasha (Gröna)",
-        },        
-        {
-            seat: 8,
-            name: "Indigo Van (Vihr)",
-        },        
-        {
-            seat: 9,
-            name: "Oiva Hasan (SDP)",
-        },        
-        {
-            seat: 10,
-            name: "Elof Knut (SDP)",
-        },        
-        {
-            seat: 11,
-            name: "Asseri Dennis (Vänst.)",
-        },        
-        {
-            seat: 12,
-            name: "Marian Catherine",
-        },        
-        {
-            seat: 13,
-            name: "Soili Unna",
-        },        
+    const { meetingId, caseNumber } = props
 
-    ]
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`#--API_URL--#/seats/${meetingId}/${caseNumber}`)
+            console.log("response", response)
+            if (response.status === 200) {
+                const data = await response.json();
+                console.log("data", data)
+                setSeats(data)
+            }
+        }
+        fetchData()
+    }, [setSeats])
 
-    seats[91] = {
-        seat: 91,
-        name: "Diana Rose (Vihr.)",
-    }
+    useEffect(() => {
 
-    seats[101] = {
-        seat: 101,
-        name: "Venni Utu (Pri)",
-    }
+        const tempSeatMap = []
+        seats.forEach(seat => {
+            if (!isNaN(seat.seatId)) {
+                tempSeatMap[Number(seat.seatId)] = {
+                    name: seat.personFI
+                }
+            }
+        })
 
-    seats[102] = {
-        seat: 102,
-        name: "Eeti Van",
-    }
-
-    seats[103] = {
-        seat: 103,
-        name: "Örri Killi (Apri)",
-    }
-
-    seats[104] = {
-        seat: 104,
-        name: "Nikola Jamie (Apri)",
-    }
-
-    seats[105] = {
-        seat: 105,
-        name: "Salad Toive (Apri)",
-    }
-
-    seats[106] = {
-        seat: 106,
-        name: "Raisu Ronji (Apri)",
-    }
+        setSeatMap(tempSeatMap)
+        console.log("tempSeatMap", tempSeatMap)
+    }, [seats])
 
     return (
         <div style={champerStyle}>
-            <SeatRow seats={seats}></SeatRow>
-            <SeatRow rowNr={0} seats={seats}></SeatRow>
-            <SeatRow rowNr={1} seats={seats}></SeatRow>
-            <SeatRow rowNr={2} seats={seats}></SeatRow>
-            <SeatRow rowNr={3} seats={seats}></SeatRow>
-            <SeatRow rowNr={4} seats={seats}></SeatRow>
-            <SeatRow rowNr={5} seats={seats}></SeatRow>
-            <SeatRow rowNr={6} seats={seats}></SeatRow>
-            <SeatRow rowNr={7} seats={seats}></SeatRow>
-            <SeatRow rowNr={8} seats={seats}></SeatRow>
-            <SeatRow rowNr={10} seats={seats} isQuest={true}></SeatRow>
+            <SeatRow seats={seatMap}></SeatRow>
+            <SeatRow rowNr={0} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={1} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={2} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={3} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={4} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={5} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={6} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={7} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={8} seats={seatMap}></SeatRow>
+            <SeatRow rowNr={10} seats={seatMap} isQuest={true}></SeatRow>
         </div>
     )
 }
