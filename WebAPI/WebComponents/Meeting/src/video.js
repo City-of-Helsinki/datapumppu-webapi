2b4a249aed268955b90a0512faf67d84a07e65c4
Ville Palo <ville.palo@avanio.com>
2023-01-30 14:39:00 +0200

    radiant player info

--- WebAPI/WebComponents/Meeting/src/video.js
+++ WebAPI/WebComponents/Meeting/src/video.js
@@ -2,7 +2,11 @@
 export default function GetVideoPosition () {
     const playerApi = new window.RadiantMP("#odPlayer")
     if (playerApi) {
-        console.log("playerApi.video.time", playerApi.getCurrentTime())
         videoPosition = playerApi.getCurrentTime()
+        console.log("getCurrentTime", videoPosition)
+        console.log("getReady", playerApi.getReady())
+        console.log("getPlayerVersion", playerApi.getPlayerVersion())
+        console.log("getStreamType", playerApi.getStreamType())
+        console.log("getControls", playerApi.getControls())
     }
 }
