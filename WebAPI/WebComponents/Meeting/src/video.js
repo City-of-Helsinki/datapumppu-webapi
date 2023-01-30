7174324e05fef5b0e4130b07612a2db770aad850
Ville Palo <ville.palo@avanio.com>
2023-01-30 15:14:11 +0200

    player info loging

--- WebAPI/WebComponents/Meeting/src/video.js
+++ WebAPI/WebComponents/Meeting/src/video.js
@@ -1,12 +1,16 @@
 
 export default function GetVideoPosition () {
-    const playerApi = new window.RadiantMP("#odPlayer")
-    if (playerApi) {
-        videoPosition = playerApi.getCurrentTime()
-        console.log("getCurrentTime", videoPosition)
-        console.log("getReady", playerApi.getReady())
-        console.log("getPlayerVersion", playerApi.getPlayerVersion())
-        console.log("getStreamType", playerApi.getStreamType())
-        console.log("getControls", playerApi.getControls())
+    const playerApi = new window.RadiantMP("odPlayer")
+
+    if (!playerApi) {
+        return 0
     }
+
+    const videoPosition = playerApi.getCurrentTime()
+    console.log("getCurrentTime", videoPosition)
+    console.log("getReady", playerApi.getReady())
+    console.log("getPlayerVersion", playerApi.getPlayerVersion())
+    console.log("getStreamType", playerApi.getStreamType())
+    console.log("getControls", playerApi.getControls())
+    return videoPosition;
 }
