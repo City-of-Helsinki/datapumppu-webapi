f87835b440f7b4e069b189f41299d91578cc4fd9
Veli-Matti Tikkanen <veli-matti.tikkanen@avanio.com>
2023-02-01 16:34:02 +0200

    add controller for requesting statement & reply reservations

---
+++ WebAPI/Controllers/DTOs/ReservationDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace WebAPI.Controllers.DTOs
+{
+    public class ReservationDTO
+    {
+        public string MeetingID { get; set; }
+
+        public int AgendaPoint { get; set; }
+
+        public DateTime? Timestamp { get; set; }
+
+        public string? Person { get; set; }
+
+        public int? Ordinal { get; set; }
+
+        public string? SeatID { get; set; }
+
+        public string? AdditionalInfoFI { get; set; }
+
+        public string? AdditionalInfoSV { get; set; }
+
+        public bool? Active { get; set; }
+    }
+}
---
+++ WebAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers.Filters;
+using WebAPI.StorageClient;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("reservations")]
+    [TypeFilter(typeof(WebAPIExceptionFilter))]
+    public class ReservationsController: ControllerBase
+    {
+        private readonly IStorageApiClient _storageApiClient;
+        private readonly ILogger<ReservationsController> _logger;
+
+        public ReservationsController(
+            IStorageApiClient storageApiClient,
+            ILogger<ReservationsController> logger)
+        {
+            _storageApiClient = storageApiClient;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{meetingId}/{caseNumber}")]
+        public async Task<IActionResult> GetReservations(string meetingId, string caseNumber)
+        {
+            _logger.LogInformation("Executing GetStatements()");
+            var reservations = await _storageApiClient.GetReservations(meetingId, caseNumber);
+            
+            return new OkObjectResult(reservations);
+        }
+    }
+}
