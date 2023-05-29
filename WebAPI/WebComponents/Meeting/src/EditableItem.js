e8420210b99c89476671f5858e431b901a96f534
Ville Palo <ville.palo@avanio.com>
2023-05-29 12:10:50 +0300

    Editable html field

--- WebAPI/WebComponents/Meeting/src/EditableItem.js
+++ WebAPI/WebComponents/Meeting/src/EditableItem.js
@@ -48,7 +48,8 @@ export default function EditableItem(props) {
     }
 
     const submitChanges = () => {
-        const editedHtml = repackHtml(stateToHTML(editorState.getCurrentContent()))
+        const editedPart = editorState.getCurrentContent()
+        const editedHtml = repackHtml(stateToHTML(editedPart))
         const agendaPoint = agendaItem.agendaPoint
         const request = {
             method: 'POST',
@@ -58,7 +59,7 @@ export default function EditableItem(props) {
             },
             body: JSON.stringify({
                 html: editedHtml,
-                decision: editableHTML,
+                decision: editedPart,
                 meetingId,
                 agendaPoint,
                 language
