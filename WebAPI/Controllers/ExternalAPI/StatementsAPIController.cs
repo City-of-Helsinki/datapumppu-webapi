f342bf0773027d26753fb679f2df7cc4ded24854
Heikki Riihinen <heikki.riihinen@avanio.com>
2024-10-29 12:21:01 +0200

    New endpoint to query statements

--- WebAPI/Controllers/ExternalAPI/StatementsAPIController.cs
+++ WebAPI/Controllers/ExternalAPI/StatementsAPIController.cs
@@ -31,5 +31,23 @@ namespace WebAPI.Controllers.ExternalAPI
             _logger.LogInformation($"GetStatementsByPerson {name} {year} {lang}");
             return new OkObjectResult(await _personStatementsProvider.GetStatements(name, year, lang));
         }
+
+        [HttpGet("lookup")]
+        public async Task<IActionResult> GetStatementsByPersonOrDate(
+            [FromQuery] string? names,
+            [FromQuery] string? startDate,
+            [FromQuery] string? endDate)
+        {
+            bool hasNames = !string.IsNullOrWhiteSpace(names);
+            bool hasDateRange = !string.IsNullOrWhiteSpace(startDate) && !string.IsNullOrWhiteSpace(endDate);
+
+            if (!hasNames && !hasDateRange)
+            {
+                return BadRequest("Kyselyssä tulee vähintään olla joko 'names' tai aikaväli 'startDate' ja 'endDate'");
+            }
+
+            _logger.LogInformation($"GetStatementsByPersonOrDate {names} {startDate} {endDate}");
+            return new OkObjectResult(await _personStatementsProvider.GetStatementsLookup(names, startDate, endDate));
+        }
     }
 }
