600b759b40edf0b25bb653655448949c3b6a9337
Heikki Riihinen <heikki.riihinen@avanio.com>
2025-11-03 12:58:16 +0200

    Fix GetVideoPosition function. Add function documentation

--- WebAPI/WebComponents/Meeting/src/video.js
+++ WebAPI/WebComponents/Meeting/src/video.js
@@ -1,11 +1,30 @@
 
-export default function GetVideoPosition () {
+/**
+ * Gets the current playback position of the RMP (Radiant Media Player) video player.
+ * 
+ * This function accesses the global RMP player instances and retrieves the current
+ * playback time from the first available player instance.
+ * 
+ * @returns {number} The current video position in seconds. Returns 0 if no player
+ *                   instance is available or if the player API is not accessible.
+ * 
+ * @example
+ * // Get current video position
+ * const currentTime = GetVideoPosition();
+ * console.log(`Video is at ${currentTime} seconds`);
+ * 
+ */
+export default function GetVideoPosition() {
+    const instances = window?.rmpGlobals?.rmpInstances
+    const playerApi = instances ? Object.values(instances)[0] : null
 
-    const playerApi = window?.rmpGlobals?.rmpInstances?.[0]
     if (!playerApi) {
         return 0
     }
 
-    const videoPosition = playerApi.getCurrentTime()
+    // Get current time in milliseconds
+    const videoPosition = playerApi.currentTime
+
+    // Convert milliseconds to seconds
     return videoPosition / 1000;
 }
